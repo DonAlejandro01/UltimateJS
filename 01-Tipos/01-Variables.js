d74429167d1ab0ad1460ff5013616458cd0db89a
Alejandro Perez <alejandrod.perezn@gmail.com>
2024-07-09 01:15:33 -0400

    Update

---
+++ 01-Tipos/01-Variables.js
@@ -0,0 +1,26 @@
+/*Variable reservada */
+
+let nombre = `Hola mundo `;
+let apellido;
+
+/*Cambiar contenido de variable */
+apellido = 'Perez';
+
+/*camelCase, UpperCamelCase y Snake_Case*/
+let NombreCompleto;
+let nombreCompleto;
+let nombre_completo;
+
+
+/*
+REGLAS:
+
+-Las variables no pueden ser nombras como palabras reservadas
+-No se puede anteponer un numero
+- UpperCamelCase: El inicio de cada palabra con mayusculas
+- camelCase: la primera palabra no commo mayuscula,pero el resto si (mas ocupada)
+- Snake_Case: Cada palabra separada por un guion bajo
+- Javascript es case sensitive: sabe diferenciar entre nombres similares pero con diferencia de mayusculas
+- Variables con nombre objetivos
+*/
+console.log(nombre);
---
+++ app.js
@@ -0,0 +1,5 @@
+/*Variables:
+ -Se permite almacenar informacion temporal o fija. dependiente del tipo que sea se puede modifiar dicha variable. 
+ Se almacena por bloque en ram 
+
+*/
